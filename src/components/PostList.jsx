f6a8f591db2e4cc442046eda29b84b22f889b9c5
Ambrish-M <ambrish2706@gmail.com>
2026-02-06 15:22:13 +0530

    fix bug

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { Spinner } from "react-bootstrap";
 import api from "../services/api";
 import PostCard from "./PostCard";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../context/useAuth";
 
 export default function PostList({ activeTab }) {
     const { user } = useAuth();
